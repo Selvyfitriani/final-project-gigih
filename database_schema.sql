f1a5935dbdc8f21c49f748c0e5591020babc0d08
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-09 00:16:00 +0700

    database schema

---
+++ database_schema.sql
@@ -0,0 +1,36 @@
+-- CREATE DATABASE
+CREATE DATABASE social_media_db;
+
+-- CREATE TABLE 1: USERS
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30),
+    email VARCHAR(40),
+    bio_description VARCHAR(150),
+
+    PRIMARY KEY(id)
+)
+
+-- CREATE TABLE 2: POSTS
+CREATE TABLE posts(
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    words VARCHAR(1000),
+    attachment TEXT,
+    hashtags VARCHAR(100),
+    time_post DATETIME,
+    
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)
+
+-- CREATE TABLE 3: COMMENTS
+CREATE TABLE comments (
+    id INT NOT NULL AUTO_INCREMENT,
+    post_id INT,
+    words VARCHAR(1000),
+    hashtags VARCHAR(100),
+
+    FOREIGN KEY(post_id) REFERENCES posts(id),
+    PRIMARY KEY(id)
+)

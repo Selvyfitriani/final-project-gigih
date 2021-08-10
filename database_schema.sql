c6a0ac1619995d3ba4e17aa8f467cc456e2b51ec
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-10 14:45:38 +0700

    add constraint to some attrs
    add NOT NULL constraint to some attributes
    
    Signed-off-by: Selvy Fitriani <selvyfiriani@gmail.com>

--- database_schema.sql
+++ database_schema.sql
@@ -1,37 +1,37 @@
 -- CREATE DATABASE
-CREATE DATABASE social_media_db;
+-- CREATE DATABASE social_media_db;
 
 -- CREATE TABLE 1: USERS
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(30),
-    email VARCHAR(40),
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(40) NOT NULL,
     bio_description VARCHAR(150),
 
     PRIMARY KEY(id)
-)
+);
 
 -- CREATE TABLE 2: POSTS
 CREATE TABLE posts(
     id INT NOT NULL AUTO_INCREMENT,
-    user_id INT,
-    words VARCHAR(1000),
+    user_id INT NOT NULL,
+    words VARCHAR(1000) NOT NULL,
     attachment TEXT,
     hashtags VARCHAR(100),
-    time_post DATETIME,
+    timepost DATETIME NOT NULL,
     
     PRIMARY KEY(id),
     FOREIGN KEY(user_id) REFERENCES users(id)
-)
+);
 
 -- CREATE TABLE 3: COMMENTS
 CREATE TABLE comments (
     id INT NOT NULL AUTO_INCREMENT,
-    post_id INT,
-    words VARCHAR(1000),
+    post_id INT NOT NULL,
+    words VARCHAR(1000) NOT NULL,
     hashtags VARCHAR(100),
     attachment TEXT,    
 
     FOREIGN KEY(post_id) REFERENCES posts(id),
     PRIMARY KEY(id)
-)
+);

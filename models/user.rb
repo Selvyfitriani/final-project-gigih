deddc92ae5f9bec445a4bb5fb1756d35632c00f3
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-11 15:11:39 +0700

    able to handle invalid email
    validate email with regex
    
    Signed-off-by: Selvy Fitriani <selvyfiriani@gmail.com>

--- models/user.rb
+++ models/user.rb
@@ -1,4 +1,27 @@
 class User
+
+    # Source valid email regex: https://www.youtube.com/watch?v=Ch-KRivqmzU
+    # docs of regex: https://rubular.com/
+    <<-RegexExplanation
+        1. Regex is start with '/' and end with '/', but 'i' after '/' indicate insensitive case
+        2. \A and \z at first and last respectively means regex must start and end with string.
+        2. First part of email is \A([\w+\-].?)+ 
+            a. [\w+\-] indicate first part must contain at least one characters (letter, number, underscore) and can have dash
+            b. .? indicate first part can contain at most one any single character
+            c. So, ([\w+\-].?)+ means first part of email must contain at least one character 
+        3. Second Part is @[a-z\d\-]+
+            a. Plus in the last means 'at least one'
+            b. [a-z\d\-] means second part can contains letter, any digit (\d) and dash. It's for domain
+            c. So, @[a-z\d\-]+ means second part must contain '@' followed by domain.
+        4. Last Part is (\.[a-z]+)*\.[a-z]+
+            a. \.[a-z]+ --> indicate second domain with format . followed by any letter
+            b. * means at least zero. So, (\.[a-z]+)* means at least zero second domain
+            c. \.[a-z]+ neans at least one second domain
+            d. So, (\.[a-z]+)*\.[a-z]+ means last part must contain at least second domain
+    RegexExplanation
+
+    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
     def initialize(id=nil, username, email, bio_description)
         @id = id
         @username = username
@@ -8,8 +31,12 @@ class User
 
     def valid?
         return false if @username.empty?
-        return false if @email.empty?
+        return false if (!valid_email?)
         return false if @bio_description.empty?
         return true
+    end
+
+    def valid_email? 
+        return @email =~ VALID_EMAIL_REGEX
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ describe User do
             it 'should return true' do
                 user = User.new(
                     username = 'selvyfitriani31',
-                    email = 'selvyfitriani31@gmail.com',
+                    email = "selvyfitriani31@gmail.com",  
                     bio_description = 'a learner',
                 )
                 expect(user.valid?).to eq(true)
@@ -20,6 +20,17 @@ describe User do
                     username = "",
                     email = "",
                     bio_description = "",
+                )
+                expect(user.valid?).to eq(false)
+            end
+        end
+
+        context 'when initialized with invalid email' do
+            it 'should return false' do
+                user = User.new(
+                    username = 'selvyfitriani31',
+                    email = "@gmail.com",
+                    bio_description = 'a learner',
                 )
                 expect(user.valid?).to eq(false)
             end

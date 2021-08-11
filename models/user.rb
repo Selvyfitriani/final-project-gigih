f81368fa4a08ba6a89a115083a8f002f70b4613f
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-11 16:25:44 +0700

    fix typos

--- models/user.rb
+++ models/user.rb
@@ -5,15 +5,15 @@ class User
     <<-RegexExplanation
         1. Regex is start with '/' and end with '/', but 'i' after '/' indicate insensitive case
         2. \A and \z at first and last respectively means regex must start and end with string.
-        2. First part of email is \A([\w+\-].?)+ 
+        3. First part of email is \A([\w+\-].?)+ 
             a. [\w+\-] indicate first part must contain at least one characters (letter, number, underscore) and can have dash
             b. .? indicate first part can contain at most one any single character
             c. So, ([\w+\-].?)+ means first part of email must contain at least one character 
-        3. Second Part is @[a-z\d\-]+
+        4. Second Part is @[a-z\d\-]+
             a. Plus in the last means 'at least one'
             b. [a-z\d\-] means second part can contains letter, any digit (\d) and dash. It's for domain
             c. So, @[a-z\d\-]+ means second part must contain '@' followed by domain.
-        4. Last Part is (\.[a-z]+)*\.[a-z]+
+        5. Last Part is (\.[a-z]+)*\.[a-z]+
             a. \.[a-z]+ --> indicate second domain with format . followed by any letter
             b. * means at least zero. So, (\.[a-z]+)* means at least zero second domain
             c. \.[a-z]+ neans at least one second domain

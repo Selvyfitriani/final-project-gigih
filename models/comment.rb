dfedd1c884b985367988fce2b62e83aa54ac13cf
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-25 06:30:15 +0700

    refactor comment
    fix indentation, white-spaced, etc
    
    Signed-off-by: Selvy Fitriani <selvyfiriani@gmail.com>

--- models/comment.rb
+++ models/comment.rb
@@ -1,95 +1,95 @@
-class Comment 
+class Comment
 
-    attr_accessor :user_id, :post_id, :text
+  attr_accessor :user_id, :post_id, :text
 
-    def initialize(id=nil, user_id, post_id, text)
-        @id = id
-        @user_id = user_id
-        @post_id = post_id
-        @text = text
-        @hashtags = ""
+  def initialize(user_id, post_id, text, id = nil)
+    @id = id
+    @user_id = user_id
+    @post_id = post_id
+    @text = text
+    @hashtags = ''
+  end
+
+  def valid?
+    return false unless valid_user_id?
+    return false unless valid_post_id?
+    return false unless valid_text?
+
+    true
+  end
+
+  def valid_user_id?
+    int_user_id = @user_id.to_i
+
+    return false if int_user_id <= 0
+
+    true
+  end
+
+  def valid_post_id?
+    int_post_id = @post_id.to_i
+
+    return false if int_post_id <= 0
+
+    true
+  end
+
+  def valid_text?
+    return false if @text.empty?
+    return false if @text.length > 1000
+
+    true
+  end
+
+  def generate_hashtags
+    @hashtags = ''
+
+    pieces = @text.split(' ')
+    pieces.each do |piece|
+      next if piece[0, 1] != '#'
+
+      hashtag = piece.downcase
+
+      next if @hashtags.include? hashtag
+
+      @hashtags += hashtag
+      @hashtags += ' '
     end
 
-    def valid?
-        return false if !valid_user_id?
-        return false if !valid_post_id?
-        return false if !valid_text?
-        return true
+    @hashtags
+  end
+
+  def save
+    return false unless valid?
+
+    generate_hashtags
+
+    client = create_db_client
+    client.query("INSERT INTO comments(user_id, post_id, text, hashtags)
+          VALUES(#{@user_id}, #{@post_id}, '#{@text}', '#{@hashtags}')")
+
+    true
+  end
+
+  def self.get_last_insert_id
+    client = create_db_client()
+    raw_data = client.query('SELECT MAX(id) as id FROM comments')
+
+    raw_data.each do |datum|
+      return datum['id'].to_i
+    end
+  end
+
+  def self.get_by_id(id)
+    client = create_db_client
+    raw_data = client.query("SELECT * FROM comments WHERE id = #{id}")
+
+    comment = nil
+
+    raw_data.each do |datum|
+      comment = Comment.new(datum['user_id'], datum['post_id'], datum['text'])
     end
 
-    def valid_user_id?
-        int_user_id = @user_id.to_i
-     
-        return false if int_user_id <= 0
-        return true
-    end
-
-    def valid_post_id?
-        int_post_id = @post_id.to_i
-
-        return false if int_post_id <= 0
-        return true
-    end
-
-    def valid_text?
-        return false if @text.empty?
-        return false if @text.length > 1000
-        return true
-    end
-
-    def generate_hashtags
-        @hashtags = ""
-    
-        pieces = @text.split(" ")
-        pieces.each do |piece| 
-            if piece[0, 1] == "#" 
-                hashtag = piece.downcase
-                if !@hashtags.include? hashtag
-                    @hashtags += hashtag
-                    @hashtags += " "
-                end
-            end 
-        end
-
-        @hashtags
-    end
-
-    def save
-        return false if !valid?
-
-        generate_hashtags
-    
-        client = create_db_client
-        client.query("INSERT INTO comments(user_id, post_id, text, hashtags) " + 
-            "VALUES(#{@user_id}, #{@post_id}, '#{@text}', '#{@hashtags}')")
-        
-        return true
-    end
-
-    def self.get_last_insert_id
-        client = create_db_client()
-        raw_data = client.query("SELECT MAX(id) as id FROM comments")
-
-        raw_data.each do |datum|
-            return datum["id"].to_i
-        end    
-    end
-
-    def self.get_by_id(id)
-        client = create_db_client
-        raw_data = client.query("SELECT * FROM comments WHERE id = #{id}")
-        
-        comment = nil
-
-        raw_data.each do |datum|
-            comment = Comment.new(
-                user_id = datum["user_id"], 
-                post_id = datum["post_id"],
-                text = datum["text"]
-            )
-        end
-
-        comment
-    end
-
+    comment
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,222 +1,171 @@
-require "./test_helper"
-require "./database/db_connector"
-require "./models/comment"
+require './test_helper'
+require './database/db_connector'
+require './models/comment'
 
 describe Comment do
-    before(:each) do
-        client = create_db_client
-        client.query("DELETE FROM users")
-        client.query("DELETE FROM posts")
-        client.query("DELETE FROM comments")
+  before(:each) do
+    client = create_db_client
+    client.query('DELETE FROM users')
+    client.query('DELETE FROM posts')
+    client.query('DELETE FROM comments')
+  end
+
+  describe '#valid?' do
+    context 'when given valid attributes value' do
+      it 'should return true' do
+        user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
+        user.save
+
+        post = Post.new(user.id, 'A new post', '2021-08-21 22:30:05', 1)
+
+        post.save
+
+        comment = Comment.new( user.id, post.id, 'A new comment')
+
+        expect(comment.valid?).to eq(true)
+      end
     end
 
-    describe '#valid?' do
-        context 'when given valid attributes value' do
-            it 'should return true' do
-                user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
-                user.save
-                
-                post = Post.new(user.id, "A new post", "2021-08-21 22:30:05", 1)
-                  
-                post.save
+    context 'when given user id with alphabetical form' do
+      it 'should return false' do
+        user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
+        user.save
 
-                comment = Comment.new(
-                    user_id = user.id,
-                    post_id = post.id,
-                    text = "A new comment"  
-                )
+        post = Post.new(user.id, 'A new post', '2021-08-21 22:30:05', 1)
 
-                expect(comment.valid?).to eq(true)
-            end
-        end
+        post.save
 
-        context 'when given user id with alphabetical form' do
-            it 'should return false' do
-                user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
-                user.save
-                
-                post = Post.new(user.id, "A new post", "2021-08-21 22:30:05", 1)
-                   
-                post.save
+        comment = Comment.new('a', post.id, 'A new comment')
 
-                comment = Comment.new(
-                    user_id = "a",
-                    post_id = post.id,
-                    text = "A new comment"  
-                )
-
-                expect(comment.valid?).to eq(false)
-            end
-        end
-
-        context 'when given post id with alphabetical form' do
-            it 'should return false' do
-                user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
-                user.save
-                
-                post = Post.new( user.id, "A new post", "2021-08-21 22:30:05", 1)
-                    
-                post.save
-
-                comment = Comment.new(
-                    user_id = user.id,
-                    post_id = "",
-                    text = "A new comment"  
-                )
-
-                expect(comment.valid?).to eq(false)
-            end
-        end
-
-        context 'when given empty text' do
-            it 'should return false' do
-                user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
-                user.save
-                
-                post = Post.new(user.id,"A new post", "2021-08-21 22:30:05", 1)
-                
-                post.save
-
-                comment = Comment.new(
-                    user_id = user.id,
-                    post_id = post.id,
-                    text = ""  
-                )
-
-                expect(comment.valid?).to eq(false)
-            end
-        end
-
-        context 'when given more than 1000 chars of text' do
-            it 'should return false' do
-                user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
-                user.save
-                
-                post = Post.new(user.id,"A new post", "2021-08-21 22:30:05", 1)
-                
-                post.save
-
-                comment = Comment.new(
-                    user_id = user.id,
-                    post_id = post.id,
-                    text = "A"*1001  
-                )
-
-                expect(comment.valid?).to eq(false)
-            end
-        end
+        expect(comment.valid?).to eq(false)
+      end
     end
 
-    describe '#generate_hashtags' do
-        context 'when text has no hashtags' do
-            it 'should return empty string' do
-                comment = Comment.new(
-                    user_id = 1,
-                    post_id = 1,
-                    text = "A new comment"  
-                )
-            
-                hashtags = comment.generate_hashtags()
-                expected_hashtags = ""
-                expect(hashtags).to eq(expected_hashtags)
-            end
-        end 
+    context 'when given post id with alphabetical form' do
+      it 'should return false' do
+        user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
+        user.save
 
-        context 'when text has 1 hashtag' do
-            it 'should return string with the hashtag' do
-                comment = Comment.new(
-                    user_id = 1,
-                    post_id = 1,
-                    text = "A new comment #gigih"  
-                )
+        post = Post.new(user.id, 'A new post', '2021-08-21 22:30:05', 1)
 
-                hashtags = comment.generate_hashtags
-                expected_hashtags = "#gigih "
-                expect(hashtags).to eq(expected_hashtags)
-            end
-        end
-
-        context 'when text contain multiple instances of a hashtag' do
-            it 'should return only one hashtag' do
-                comment = Comment.new(
-                    user_id = 1,
-                    post_id = 1,
-                    text = "#hashtag #hashtaG #hasHtag"
-                )
-
-                hashtags = comment.generate_hashtags()
-                expected_hashtags = "#hashtag "
-                expect(hashtags).to eq(expected_hashtags)
-            end
-        end
+        post.save
+        comment = Comment.new(user.id, '', 'A new comment')
+        expect(comment.valid?).to eq(false)
+      end
     end
 
-    describe '#save' do
-        context 'when given invalid input' do
-            it 'should not save to database and return false' do
-                comment = Comment.new(
-                    user_id = "",
-                    post_id = "",
-                    text = ""
-                )
+    context 'when given empty text' do
+      it 'should return false' do
+        user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
+        user.save
 
-                expect(comment.save).to eq(false)
-            end
-        end
+        post = Post.new(user.id,'A new post', '2021-08-21 22:30:05', 1)
 
-        context 'when given with valid input' do
-            it 'should save to database and return true' do
-                user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
-                user.save
-                
-                post = Post.new(user.id,"A new post", "2021-08-21 22:30:05", 1)
-                
-                post.save
+        post.save
+        comment = Comment.new(user.id, post.id, '')
 
-                comment = Comment.new(
-                    user_id = user.id,
-                    post_id = post.id,
-                    text = "A new comment"  
-                )
-
-                dummy_database = double
-                allow(Mysql2::Client).to receive(:new).and_return(dummy_database)
-                allow(dummy_database).to receive(:query).with("INSERT INTO comments(user_id, post_id, text, hashtags) " +
-                    "VALUES(#{comment.user_id}, #{comment.post_id}, '#{comment.text}', '')")
-
-                expect(comment.save).to eq(true)
-            end
-        end
-
-        context 'when given text with hashtags' do
-            it 'should generate all hashtags and save to database' do
-                user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
-                user.save
-                
-                post = Post.new(user.id,"A new post", "2021-08-21 22:30:05", 1)
-                
-                post.save
-
-                comment = Comment.new(
-                    user_id = user.id,
-                    post_id = post.id,
-                    text = "A new comment #gigih"  
-                )
-
-                hashtags = comment.generate_hashtags
-
-                dummy_database = double
-                allow(Mysql2::Client).to receive(:new).and_return(dummy_database)
-                allow(dummy_database).to receive(:query).with("INSERT INTO comments" +
-                    "(user_id, post_id, text, hashtags) " +
-                    "VALUES(#{comment.user_id}, #{comment.post_id}, '#{comment.text}', '#{hashtags}')")
-                
-                comment.save
-                
-                expected_hashtags = "#gigih "
-
-                expect(hashtags).to eq(expected_hashtags)
-            end
-        end
+        expect(comment.valid?).to eq(false)
+      end
     end
+
+    context 'when given more than 1000 chars of text' do
+      it 'should return false' do
+        user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
+        user.save
+
+        post = Post.new(user.id,'A new post', '2021-08-21 22:30:05', 1)
+
+        post.save
+        comment = Comment.new( user.id, post.id, 'A' * 1001)
+        expect(comment.valid?).to eq(false)
+      end
+    end
+  end
+
+  describe '#generate_hashtags' do
+    context 'when text has no hashtags' do
+      it 'should return empty string' do
+        comment = Comment.new(1, 1, 'A' * 1001)
+
+        hashtags = comment.generate_hashtags
+        expected_hashtags = ''
+        expect(hashtags).to eq(expected_hashtags)
+      end
+    end
+
+    context 'when text has 1 hashtag' do
+      it 'should return string with the hashtag' do
+        comment = Comment.new(1, 1, 'A new comment #gigih')
+
+        hashtags = comment.generate_hashtags
+        expected_hashtags = '#gigih '
+        expect(hashtags).to eq(expected_hashtags)
+      end
+    end
+
+    context 'when text contain multiple instances of a hashtag' do
+      it 'should return only one hashtag' do
+        comment = Comment.new(1, 1, '#hashtag #hashtaG #hasHtag')
+
+        hashtags = comment.generate_hashtags()
+        expected_hashtags = '#hashtag '
+        expect(hashtags).to eq(expected_hashtags)
+      end
+    end
+  end
+
+  describe '#save' do
+    context 'when given invalid input' do
+      it 'should not save to database and return false' do
+        comment = Comment.new('', '', '')
+
+        expect(comment.save).to eq(false)
+      end
+    end
+
+    context 'when given with valid input' do
+      it 'should save to database and return true' do
+        user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
+        user.save
+
+        post = Post.new(user.id, 'A new post', '2021-08-21 22:30:05', 1)
+
+        post.save
+
+        comment = Comment.new(user.id, post.id, 'A new comment')
+
+        dummy_database = double
+        allow(Mysql2::Client).to receive(:new).and_return(dummy_database)
+        allow(dummy_database).to receive(:query).with("INSERT INTO comments(user_id, post_id, text, hashtags)
+          VALUES(#{comment.user_id}, #{comment.post_id}, '#{comment.text}', '')")
+
+        expect(comment.save).to eq(true)
+      end
+    end
+
+    context 'when given text with hashtags' do
+      it 'should generate all hashtags and save to database' do
+        user = User.new('selvyfitriani31', 'selvyfitriani31@gmail.com', 'a learner', 1)
+        user.save
+
+        post = Post.new(user.id,'A new post', '2021-08-21 22:30:05', 1)
+
+        post.save
+
+        comment = Comment.new(user.id, post.id, 'A new comment #gigih')
+        hashtags = comment.generate_hashtags
+
+        dummy_database = double
+        allow(Mysql2::Client).to receive(:new).and_return(dummy_database)
+        allow(dummy_database).to receive(:query).with("INSERT INTO comments(user_id, post_id, text, hashtags)
+          VALUES(#{comment.user_id}, #{comment.post_id}, '#{comment.text}', '#{hashtags}')")
+
+        comment.save
+
+        expected_hashtags = '#gigih '
+
+        expect(hashtags).to eq(expected_hashtags)
+      end
+    end
+  end
 end

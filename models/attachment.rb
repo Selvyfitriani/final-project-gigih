eccbee77b64c705eff7f1f9085cff35fda50bc5a
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-25 09:55:03 +0700

    improving validation method for attachment
    able to handle if one attachment owned by post and comment simultaneously
    
    Signed-off-by: Selvy Fitriani <selvyfiriani@gmail.com>

--- models/attachment.rb
+++ models/attachment.rb
@@ -2,8 +2,8 @@ class Attachment
 
   def initialize(filename, type, post_id = nil, comment_id = nil, id = nil)
     @filename = filename
-    @type = type,
-    @post_id = post_id,
+    @type = type
+    @post_id = post_id
     @comment_id = comment_id
     @id = id
   end
@@ -11,7 +11,21 @@ class Attachment
   def valid?
     return false if @filename.empty?
     return false if @type.empty?
+    return false unless valid_post_id? ^ valid_comment_id?
 
     true
   end
+
+  def valid_post_id?
+    return true if @post_id.to_i.positive?
+
+    false
+  end
+
+  def valid_comment_id?
+    return true if @comment_id.to_i.positive?
+
+    false
+  end
+
 end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -9,5 +9,13 @@ describe Attachment do
         expect(attachment.valid?).to eq(true)
       end
     end
+
+    context 'when given post and comment id simultaneously' do
+      it 'should return false' do
+        attachment = Attachment.new('filename.png', 'image/png', 1, 1)
+
+        expect(attachment.valid?).to eq(false)
+      end
+    end
   end
 end
